c47e7939e74ca834a3c2761dbd9baab62f6f923a
Say_Me <66777357+SayMeAlound@users.noreply.github.com>
2021-02-26 00:07:21 +0800

    ABA问题解决 乐观锁

--- src/CAS/CASDemo.java
+++ src/CAS/CASDemo.java
@@ -3,6 +3,7 @@ package CAS;/*
     @create 2021-02-25-22:44  
 */
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicStampedReference;
@@ -13,25 +14,69 @@ import java.util.concurrent.atomic.AtomicStampedReference;
             1.自旋锁  循环会耗时
             2.一次性只能保证一个共享变量的原子性
             3.ABA问题
+        解决ABA问题   AtomicStampedReference  思想 乐观锁
  */
 public class CASDemo {
     //CAS  compareAndSet  比较并交换
+
+    // AtomicStampedReference Integer 注意Integer 包装类的值范围  -128  -127  注意对象引用问题
+    //正常业务操作 这里面比较的都是一个个对象
+    static AtomicStampedReference<Integer> atomicStampedReference = new AtomicStampedReference<>(1, 1);
     public static void main(String[] args) {
-        AtomicInteger atomicInteger =new  AtomicInteger(2021);
+//        AtomicInteger atomicInteger =new  AtomicInteger(2021);
 
-        //对于我们写的SQL来说   : 乐观锁!
-        // 期望  更新
-        // public final boolean compareAndSet(int expect, int update) {
-        // 如果期望值达到了  就更新 否则就不更新  CAS 是CPU的并发原语
-        // ============捣乱线程 ===========
-        System.out.println(atomicInteger.compareAndSet(2021, 2022));
-        System.out.println(atomicInteger.get());
 
-        System.out.println(atomicInteger.compareAndSet(2022, 2021));
-        System.out.println(atomicInteger.get());
-        // ============期望线程 ===========
-        System.out.println(atomicInteger.compareAndSet(2021, 6666));
-        System.out.println(atomicInteger.get());
+        new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();   //获得版本号
+            System.out.println("A1=> "+stamp);
+//            try {
+//                TimeUnit.SECONDS.sleep(2);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            // Version +1
+            System.out.println(atomicStampedReference.compareAndSet(
+                    1, 2, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1));
+            System.out.println("A2=> "+atomicStampedReference.getStamp()+"更改为2");
+
+
+            System.out.println(atomicStampedReference.compareAndSet(
+                    2, 1, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1));
+            System.out.println("A3=> "+atomicStampedReference.getStamp()+"更改为1");
+
+        },"A").start();
+
+
+        new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();   //获得版本号
+            System.out.println("B1=> "+stamp);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(atomicStampedReference.compareAndSet(1, 120, stamp, stamp + 1));
+            System.out.println("B2=> "+atomicStampedReference.getStamp()+"更改为6");
+        },"B").start();
+
+
+
+
+
+
+//        //对于我们写的SQL来说   : 乐观锁!
+//        // 期望  更新
+//        // public final boolean compareAndSet(int expect, int update) {
+//        // 如果期望值达到了  就更新 否则就不更新  CAS 是CPU的并发原语
+//        // ============捣乱线程 ===========
+//        System.out.println(atomicInteger.compareAndSet(2021, 2022));
+//        System.out.println(atomicInteger.get());
+//
+//        System.out.println(atomicInteger.compareAndSet(2022, 2021));
+//        System.out.println(atomicInteger.get());
+//        // ============期望线程 ===========
+//        System.out.println(atomicInteger.compareAndSet(2021, 6666));
+//        System.out.println(atomicInteger.get());
 
     }
 }
